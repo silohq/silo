5880195619a8a78c6a8e90867833fc15b08a25c4
mar-tina <marr.tina344@gmail.com>
2022-01-12 16:12:23 +0300

    feat: traverse struct

--- read_struct.go
+++ read_struct.go
@@ -4,22 +4,24 @@ import (
 	"reflect"
 )
 
-func ReadStruct(st interface{}) {
-	val := reflect.ValueOf(st)
+func StructRead(in interface{}) {
+	val := extract(in)
+	for i := 0; i < val.NumField(); i++ {
+		f := val.Field(i)
+		//currently only checking for slice type
+		if f.Kind() == reflect.Slice {
+			for j := 0; j < f.Len(); j++ {
+				StructRead(f.Index(j).Interface())
+			}
+		}
+	}
+}
+
+func extract(in interface{}) reflect.Value {
+	val := reflect.ValueOf(in)
 	if val.Kind() == reflect.Ptr {
 		val = val.Elem()
 	}
-	for i := 0; i < val.NumField(); i++ {
-		f := val.Field(i)
-		switch f.Kind() {
-		case reflect.Struct:
-			ReadStruct(f.Interface())
-		case reflect.Slice:
-			for j := 0; j < f.Len(); j++ {
-				ReadStruct(f.Index(j).Interface())
-			}
-		case reflect.String:
-			// fmt.Printf("%v=%v\n", val.Type().Field(i).Name, val.Field(i).Interface())
-		}
-	}
+
+	return val
 }
