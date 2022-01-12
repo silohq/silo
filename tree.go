266b892eaa6ea9ff8f7e002c46da305988b0d3fc
mar-tina <marr.tina344@gmail.com>
2022-01-12 16:11:32 +0300

    tree definition complete

--- tree.go
+++ tree.go
@@ -1,19 +1,18 @@
 package silo
 
 type Tree struct {
-	T []Doc `json:"tree"`
+	T []*Doc `json:"tree"`
 }
 
 type Doc struct {
-	Type  string  `json:"type"`
-	Label string  `json:"label,omitempty"`
 	Nodes []*Node `json:"nodes"`
 }
 
 type Node struct {
-	Parent string `json:"parent"`
-	Type   string `json:"type"`
-	Kind   string `json:"kind"`
-	Unique string `json:"unique"`
-	Label  string `json:"label"`
+	Parent string  `json:"parent"`
+	Type   string  `json:"type"`
+	Kind   string  `json:"kind"`
+	Unique bool    `json:"unique"`
+	Label  string  `json:"label"`
+	Nodes  []*Node `json:"nodes"`
 }
